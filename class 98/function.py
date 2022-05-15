a34e113995f6861fadbcd1fdb6b1a35a5657ec8a
AGTeeV <81072817+AGTeeV@users.noreply.github.com>
2022-05-15 19:29:46 +0530

    Delete class 98 directory

--- class 98/function.py
+++
@@ -1,9 +0,0 @@
-def countWordsFromFile():
-    fileName=input("enter the file name = ")
-    file=open(fileName,"r")
-    words=0
-    for line in file:
-        w=line.split()
-        words=words+len(w)
-    print("words=",words)
-countWordsFromFile()
